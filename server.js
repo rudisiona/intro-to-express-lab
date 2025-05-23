1e62e0056baf39ff05c94af54d8971952a4444ae
rudisiona <rsmageo@gmail.com>
2025-05-22 20:50:29 -0700

    rolled dice

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 
 
+app.get('/greetings/:username', (req, res) => {
+    res.send(`<h1>Welcome, ${req.params.username}! Nice to have you again.`)
+})
+
+app.get('/roll/:number', (req, res) => {
+    const min = 1;
+    const max = parseInt(req.params.number)
+    const rolledDice = Math.floor(Math.random() * (max) + min)
+    res.send(`You rolled a ${rolledDice}`)
+})
+
+
+
 app.listen(3000, () => {
     console.log('reporting from port 3000')
 })
